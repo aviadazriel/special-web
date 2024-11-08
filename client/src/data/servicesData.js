2c68a5b9f0ca9cf504fb9cafaf8bbca4aed95a19
Aviad Azriel <aviadazriel2@gmail.com>
2024-11-08 18:25:12 +0200

    services data

---
+++ client/src/data/servicesData.js
@@ -0,0 +1,43 @@
+// servicesData.js
+const services = [
+    {
+      id: 1,
+      title: "תכנון משכנתא",
+      description: "שירותי תכנון מקיפים למשכנתא שלך.",
+      image: "https://mashkantaguru.co.il/wp-content/uploads/2019/03/mashkanta-hol-430x330.jpg",
+    },
+    {
+      id: 2,
+      title: "מיחזור הלוואות",
+      description: "סיוע במציאת אפשרויות מיחזור טובות יותר.",
+      image: "https://www.hon.co.il/wp-content/uploads/2016/03/mortgage-351x480.jpg",
+    },
+    {
+      id: 3,
+      title: "איחוד חובות",
+      description: "אסטרטגיות לאיחוד וניהול חובות בצורה יעילה.",
+      image: "https://www.hon.co.il/wp-content/uploads/2011/11/%D7%9E%D7%93%D7%A8%D7%99%D7%9B%D7%99-%D7%9E%D7%A9%D7%9B%D7%A0%D7%AA%D7%90%D7%95%D7%AA-55x55-1.jpg",
+    },
+    {
+        id: 4,
+        title: "תכנון משכנתא",
+        description: "שירותי תכנון מקיפים למשכנתא שלך.",
+        image: "https://mashkantaguru.co.il/wp-content/uploads/2019/03/mashkanta-hol-430x330.jpg",
+      },
+      {
+        id: 5,
+        title: "מיחזור הלוואות",
+        description: "סיוע במציאת אפשרויות מיחזור טובות יותר.",
+        image: "https://www.hon.co.il/wp-content/uploads/2016/03/mortgage-351x480.jpg",
+      },
+      {
+        id: 6,
+        title: "איחוד חובות",
+        description: "אסטרטגיות לאיחוד וניהול חובות בצורה יעילה.",
+        image: "https://www.hon.co.il/wp-content/uploads/2011/11/%D7%9E%D7%93%D7%A8%D7%99%D7%9B%D7%99-%D7%9E%D7%A9%D7%9B%D7%A0%D7%AA%D7%90%D7%95%D7%AA-55x55-1.jpg",
+      },
+    // Add additional services as needed
+  ];
+  
+  export default services;
+  
--- client/src/pages/Services.js
+++ client/src/pages/Services.js
@@ -1,54 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 import ServiceCard from '../components/ServiceCard';
+import services from '../data/servicesData'; // Import services data
 
 const Services = () => (
   <Container>
-    <ServiceCard
-      title="תכנון משכנתא"
-      description="שירותי תכנון מקיפים למשכנתא שלך."
-      image="https://mashkantaguru.co.il/wp-content/uploads/2019/03/mashkanta-hol-430x330.jpg"
-    />
-    <ServiceCard
-      title="מיחזור הלוואות"
-      description="סיוע במציאת אפשרויות מיחזור טובות יותר."
-      image="https://www.hon.co.il/wp-content/uploads/2016/03/mortgage-351x480.jpg"
-    />
-    <ServiceCard
-      title="איחוד חובות"
-      description="אסטרטגיות לאיחוד וניהול חובות בצורה יעילה."
-      image="https://www.hon.co.il/wp-content/uploads/2011/11/%D7%9E%D7%93%D7%A8%D7%99%D7%9B%D7%99-%D7%9E%D7%A9%D7%9B%D7%A0%D7%AA%D7%90%D7%95%D7%AA-55x55-1.jpg"
-    />
-    <ServiceCard
-      title="תכנון משכנתא"
-      description="שירותי תכנון מקיפים למשכנתא שלך."
-      image="https://mashkantaguru.co.il/wp-content/uploads/2019/03/mashkanta-hol-430x330.jpg"
-    />
-    <ServiceCard
-      title="מיחזור הלוואות"
-      description="סיוע במציאת אפשרויות מיחזור טובות יותר."
-      image="https://www.hon.co.il/wp-content/uploads/2016/03/mortgage-351x480.jpg"
-    />
-    <ServiceCard
-      title="איחוד חובות"
-      description="אסטרטגיות לאיחוד וניהול חובות בצורה יעילה."
-      image="https://www.hon.co.il/wp-content/uploads/2011/11/%D7%9E%D7%93%D7%A8%D7%99%D7%9B%D7%99-%D7%9E%D7%A9%D7%9B%D7%A0%D7%AA%D7%90%D7%95%D7%AA-55x55-1.jpg"
-    />
-    <ServiceCard
-      title="תכנון משכנתא"
-      description="שירותי תכנון מקיפים למשכנתא שלך."
-      image="https://mashkantaguru.co.il/wp-content/uploads/2019/03/mashkanta-hol-430x330.jpg"
-    />
-    <ServiceCard
-      title="מיחזור הלוואות"
-      description="סיוע במציאת אפשרויות מיחזור טובות יותר."
-      image="https://www.hon.co.il/wp-content/uploads/2016/03/mortgage-351x480.jpg"
-    />
-    <ServiceCard
-      title="איחוד חובות"
-      description="אסטרטגיות לאיחוד וניהול חובות בצורה יעילה."
-      image="https://www.hon.co.il/wp-content/uploads/2011/11/%D7%9E%D7%93%D7%A8%D7%99%D7%9B%D7%99-%D7%9E%D7%A9%D7%9B%D7%A0%D7%AA%D7%90%D7%95%D7%AA-55x55-1.jpg"
-    />
+    {services.map(service => (
+      <ServiceCard
+        key={service.id}
+        title={service.title}
+        description={service.description}
+        image={service.image}
+      />
+    ))}
   </Container>
 );
 
