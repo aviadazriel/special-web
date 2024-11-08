a40999ee6ec4d521b8e07ed038167660fa8bc99d
Aviad Azriel <aviadazriel2@gmail.com>
2024-11-08 18:38:54 +0200

    news data

---
+++ client/src/data/newsData.js
@@ -0,0 +1,70 @@
+
+
+const news = [
+    {
+      id: 1,
+      title: 'הדירות החדשות בת"א מגיעות במחירים מפתיעים',
+      link: 'https://www.calcalist.co.il/real-estate/article/r1ens4yzke',
+      description: 'כתבה מרתקת על מחירי הנדל"ן בתל אביב ושינויים בשוק.',
+      imageUrl: 'https://pic1.calcalist.co.il/picserver3/crop_images/2024/11/06/HJ39gGFb1x/HJ39gGFb1x_0_0_2500_1667_0_x-large.jpg', // Replace with actual image if available
+      source: 'Calcalist',
+      publishDate: '2023-10-01' // Replace with actual publish date if available
+    },
+    {
+      id: 2,
+      title: 'שוק הנדל"ן בתנודתיות: מה צופן העתיד?',
+      link: 'https://www.calcalist.co.il/investing/article/rjegyri70',
+      description: ' לקוחות משלמים 70 אלף שקל בשנה לבנק למשכנתא, ולמרות זאת חייבים אותו הסכום',
+      imageUrl: 'https://pic1.calcalist.co.il/picserver3/crop_images/2023/02/27/rkKfzJ5Rj/rkKfzJ5Rj_1_0_1000_668_0_x-large.jpg',
+      source: 'Calcalist',
+      publishDate: '2023-09-25'
+    },
+    {
+      id: 3,
+      title: 'תחזיות כלכליות לשוק הדיור בישראל',
+      link: 'https://www.calcalist.co.il/investing/article/hyb8hnokje',
+      description: 'שוק הדיור בוער: זינוק של 32% במשכנתאות בספטמבר לעומת הממוצע',
+      imageUrl: 'https://pic1.calcalist.co.il/picserver3/crop_images/2024/09/05/HkhdvbPnR/HkhdvbPnR_0_0_2000_1333_0_x-large.jpg',
+      source: 'Calcalist',
+      publishDate: '2023-09-15'
+    },
+    {
+      id: 4,
+      title: 'ירידות מחירים משמעותיות: האם מדובר במגמה?',
+      link: 'https://www.globes.co.il/news/article.aspx?did=1001493211',
+      description: 'סקירה על הירידות האחרונות במחירי הנדל"ן בארץ.',
+      imageUrl: 'https://images.globes.co.il/dynamic/images/source/04price_drop_trend.jpg',
+      source: 'Globes',
+      publishDate: '2023-09-05'
+    },
+    {
+      id: 5,
+      title: 'משקיעים ותיקים משנים גישה בשוק',
+      link: 'https://www.calcalist.co.il/article/r1hoki1w1e',
+      description: 'מדוע משקיעים ותיקים עוברים לגישות חדשות בתחום ההשקעות.',
+      imageUrl: 'https://images.calcalist.co.il/dynamic/images/source/05investor_trends.jpg',
+      source: 'Calcalist',
+      publishDate: '2023-08-28'
+    },
+    {
+      id: 6,
+      title: 'דעות חדשות על שוק הנדל"ן בשנה הקרובה',
+      link: 'https://www.ynet.co.il/economy/article/sjxfcy7dr',
+      description: 'מומחים חולקים דעותיהם על תחזיות הנדל"ן לשנה הקרובה.',
+      imageUrl: 'https://ynet-pic1.yit.co.il/cdn-cgi/image/f=auto,w=740,q=75/picserver5/crop_images/2021/05/11/H1biM2wO00/H1biM2wO00_0_0_1000_665_0_x-large.jpg',
+      source: 'Ynet',
+      publishDate: '2023-08-20'
+    },
+    {
+      id: 7,
+      title: 'ההשפעות הכלכליות של מגמת העליות בהשקעות נדל"ן',
+      link: 'https://www.israelhayom.co.il/business/article/16446438',
+      description: 'ניתוח על ההשפעות הכלכליות של העלייה בהשקעות נדל"ן.',
+      imageUrl: 'https://www.israelhayom.co.il/wp-content/uploads/2024/09/15/15/DSC_4930-960x640.jpg',
+      source: 'Israel Hayom',
+      publishDate: '2023-08-10'
+    }
+  ];
+
+
+export default news;
