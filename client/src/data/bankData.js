e53eb19d9a586bd5409c2cc93e11565b14201e2d
Aviad Azriel <aviadazriel2@gmail.com>
2024-11-10 00:16:18 +0200

    banks

---
+++ client/src/data/bankData.js
@@ -0,0 +1,30 @@
+const banks = [
+        { url: "https://www.hfca.org.il/app/bankers/get_bankers/11", img: "https://upload.wikimedia.org/wikipedia/he/7/7e/DiscountBank.svg", alt: "בנק דיסקונט לישראל" },
+        { url: "https://www.hfca.org.il/app/bankers/get_bankers/31", img: "https://upload.wikimedia.org/wikipedia/he/5/51/%D7%94%D7%91%D7%A0%D7%A7_%D7%94%D7%91%D7%99%D7%A0%D7%9C%D7%90%D7%95%D7%9E%D7%99.svg", alt: "בנק הבינלאומי הראשון" },
+        { url: "https://www.hfca.org.il/app/bankers/get_bankers/12", img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/BankHapoalim.svg/467px-BankHapoalim.svg.png", alt: "בנק הפועלים" },
+        { url: "https://www.hfca.org.il/app/bankers/get_bankers/4", img: "https://upload.wikimedia.org/wikipedia/he/d/da/%D7%91%D7%A0%D7%A7_%D7%99%D7%94%D7%91.svg", alt: "בנק יהב לעובדי המדינה" },
+        { url: "https://www.hfca.org.il/app/bankers/get_bankers/54", img: "https://upload.wikimedia.org/wikipedia/he/6/65/%D7%91%D7%A0%D7%A7_%D7%99%D7%A8%D7%95%D7%A9%D7%9C%D7%99%D7%9D.svg", alt: "בנק ירושלים" },
+        { url: "https://www.hfca.org.il/app/bankers/get_bankers/10", img: "https://upload.wikimedia.org/wikipedia/he/3/36/BankLeumiLogo.svg", alt: "בנק לאומי" },
+        { url: "https://www.hfca.org.il/app/bankers/get_bankers/20", img: "https://upload.wikimedia.org/wikipedia/commons/6/62/%D7%9C%D7%95%D7%92%D7%95_%D7%A9%D7%9C_%D7%91%D7%A0%D7%A7_%D7%9E%D7%96%D7%A8%D7%97%D7%99-%D7%98%D7%A4%D7%97%D7%95%D7%AA.svg", alt: "בנק מזרחי טפחות" },
+        { url: "https://www.hfca.org.il/app/bankers/get_bankers/46", img: "https://upload.wikimedia.org/wikipedia/he/c/ca/MassadBank.svg", alt: "בנק מסד" },
+        { url: "https://www.hfca.org.il/app/bankers/get_bankers/17", img: "https://upload.wikimedia.org/wikipedia/he/c/c7/Mercantile.svg", alt: "בנק מרכנתיל דיסקונט" },
+        { url: "https://www.hfca.org.il/app/bankers/get_bankers/14", img: "https://upload.wikimedia.org/wikipedia/he/0/0f/%D7%91%D7%A0%D7%A7_%D7%90%D7%95%D7%A6%D7%A8_%D7%94%D7%97%D7%99%D7%99%D7%9C.svg", alt: "בנק אוצר החייל" },
+        // { url: "https://www.hfca.org.il/app/bankers/get_bankers/52", img: "https://upload.wikimedia.org/wikipedia/he/1/1f/%D7%91%D7%A0%D7%A7_%D7%A4%D7%90%D7%92%D7%99.svg", alt: "בנק פאגי" },
+        // { url: "https://www.hfca.org.il/app/bankers/get_bankers/1000", img: "https://upload.wikimedia.org/wikipedia/he/5/54/AyalonIns.svg", alt: "איילון" },
+        // { url: "https://www.hfca.org.il/app/bankers/get_bankers/1001", img: "https://wecheck-images-prod-eu-west-1.s3.eu-west-1.amazonaws.com/origin/master/static_content/dashboard/shared/stand-together-logo.svg", alt: "וויצ'ק" },
+        // { url: "https://www.hfca.org.il/app/bankers/get_bankers/1002", img: "https://upload.wikimedia.org/wikipedia/he/3/31/%D7%9C%D7%95%D7%92%D7%95_%D7%9E%D7%99%D7%98%D7%91_%D7%91%D7%99%D7%AA_%D7%94%D7%A9%D7%A7%D7%A2%D7%95%D7%AA.svg", alt: "מיטב מימון" },
+        // { url: "https://www.hfca.org.il/app/bankers/get_bankers/1003", img: "https://upload.wikimedia.org/wikipedia/he/3/3d/Mimun-yashir-logo.png", alt: "מימון ישיר" },
+        // { url: "https://www.hfca.org.il/app/bankers/get_bankers/1004", img: "https://upload.wikimedia.org/wikipedia/commons/9/95/Tarya_Logo.jpg", alt: "טריא" },
+        // { url: "https://www.hfca.org.il/app/bankers/get_bankers/1005", img: "https://www.dunsguide.co.il/Images/CompanyImages/600349582n9.gif", alt: "קווליטי" },
+        // { url: "https://www.hfca.org.il/app/bankers/get_bankers/1006", img: "https://credito.co.il/wp-content/uploads/2019/01/logo-color-new-size.png", alt: "קרדיטו" },
+        // { url: "https://www.hfca.org.il/app/bankers/get_bankers/1007", img: "https://upload.wikimedia.org/wikipedia/he/4/43/%D7%9E%D7%A0%D7%95%D7%A8%D7%94_%D7%9E%D7%91%D7%98%D7%97%D7%99%D7%9D_%D7%9C%D7%95%D7%92%D7%95.svg", alt: "מנורה משכנתאות" },
+        // { url: "https://www.hfca.org.il/app/bankers/get_bankers/1008", img: "https://upload.wikimedia.org/wikipedia/commons/d/d2/%D7%9C%D7%95%D7%92%D7%95_%D7%9B%D7%9C%D7%9C_%D7%91%D7%99%D7%98%D7%95%D7%97.svg", alt: "כלל מימון ומשכנתאות" },
+        // { url: "https://www.hfca.org.il/app/bankers/get_bankers/1009", img: "https://upload.wikimedia.org/wikipedia/he/2/25/FnxLogo.svg", alt: "הפניקס משכנתאות זהב" },
+        // { url: "https://www.hfca.org.il/app/bankers/get_bankers/1010", img: "https://www.credit360.co.il/wp-content/uploads/2022/10/logo.d110a0.webp", alt: "קרדיט 360" },
+        // { url: "https://www.hfca.org.il/app/bankers/get_bankers/1011", img: "https://upload.wikimedia.org/wikipedia/commons/6/63/BTB_logo.png", alt: "בי טי בי" },
+        // { url: "https://www.hfca.org.il/app/bankers/get_bankers/1012", img: "https://itga.org.il/wp-content/uploads/2016/06/%D7%9C%D7%95%D7%92%D7%95-%D7%94%D7%A8%D7%90%D7%9C.jpg", alt: "הראל" },
+        // { url: "https://www.hfca.org.il/app/bankers/get_bankers/1013", img: "https://static.wixstatic.com/media/81380e_7691ca489b8645b7a0f354d7d1b9a686~mv2.png/v1/crop/x_364,y_268,w_501,h_328/fill/w_144,h_94,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Artboard%208%20copy%204_4x.png", alt: "קרן גשרים" },
+        // { url: "https://www.hfca.org.il/app/bankers/get_bankers/1014", img: "https://doi.co.il/wp-content/uploads/2020/07/doi-email-logo.png", alt: "די או איי" }
+      ];
+
+      export default banks;
