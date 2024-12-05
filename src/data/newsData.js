270df01d006a536caa8bf5af173d1ccd9c6efb6b
Aviad Azriel <aviadazriel2@gmail.com>
2024-12-05 23:44:06 +0200

    ggd

--- src/data/newsData.js
+++ src/data/newsData.js
@@ -1,6 +1,7 @@
 
 
 const news = [
+ 
     {
       id: 1,
       title: 'הדירות החדשות בת"א מגיעות במחירים מפתיעים',
@@ -8,7 +9,7 @@ const news = [
       description: 'כתבה מרתקת על מחירי הנדל"ן בתל אביב ושינויים בשוק.',
       imageUrl: 'https://pic1.calcalist.co.il/picserver3/crop_images/2024/11/06/HJ39gGFb1x/HJ39gGFb1x_0_0_2500_1667_0_x-large.jpg', // Replace with actual image if available
       source: 'Calcalist',
-      publishDate: '2023-10-01' // Replace with actual publish date if available
+      publishDate: '2024-11-07' // Replace with actual publish date if available
     },
     {
       id: 2,
@@ -17,7 +18,7 @@ const news = [
       description: ' לקוחות משלמים 70 אלף שקל בשנה לבנק למשכנתא, ולמרות זאת חייבים אותו הסכום',
       imageUrl: 'https://pic1.calcalist.co.il/picserver3/crop_images/2023/02/27/rkKfzJ5Rj/rkKfzJ5Rj_1_0_1000_668_0_x-large.jpg',
       source: 'Calcalist',
-      publishDate: '2023-09-25'
+      publishDate: '2024-05-19'
     },
     {
       id: 3,
@@ -26,7 +27,7 @@ const news = [
       description: 'שוק הדיור בוער: זינוק של 32% במשכנתאות בספטמבר לעומת הממוצע',
       imageUrl: 'https://pic1.calcalist.co.il/picserver3/crop_images/2024/09/05/HkhdvbPnR/HkhdvbPnR_0_0_2000_1333_0_x-large.jpg',
       source: 'Calcalist',
-      publishDate: '2023-09-15'
+      publishDate: '2024-10-15'
     },
     {
       id: 4,
@@ -35,7 +36,7 @@ const news = [
       description: 'סקירה על הירידות האחרונות במחירי הנדל"ן בארץ.',
       imageUrl: 'https://res.cloudinary.com/globes/image/upload/t_800X392,f_auto/v1622384296/direct/shutterstock_1923763337_p9tt4d.jpg',
       source: 'Globes',
-      publishDate: '2023-09-05'
+      publishDate: '2024-11-05'
     },
     {
       id: 5,
@@ -44,7 +45,7 @@ const news = [
       description: 'מדוע משקיעים ותיקים עוברים לגישות חדשות בתחום ההשקעות.',
       imageUrl: 'https://pic1.calcalist.co.il/picserver3/wcm_upload_dev/2022/09/15/Hk9OzwlWi/calcalistlogn.png',
       source: 'Calcalist',
-      publishDate: '2023-08-28'
+      publishDate: '2024-11-03'
     },
     {
       id: 6,
@@ -53,7 +54,7 @@ const news = [
       description: 'מומחים חולקים דעותיהם על תחזיות הנדל"ן לשנה הקרובה.',
       imageUrl: 'https://ynet-pic1.yit.co.il/cdn-cgi/image/f=auto,w=740,q=75/picserver5/crop_images/2021/05/11/H1biM2wO00/H1biM2wO00_0_0_1000_665_0_x-large.jpg',
       source: 'Ynet',
-      publishDate: '2023-08-20'
+      publishDate: '2024-07-16'
     },
     {
       id: 7,
@@ -62,8 +63,18 @@ const news = [
       description: 'ניתוח על ההשפעות הכלכליות של העלייה בהשקעות נדל"ן.',
       imageUrl: 'https://www.israelhayom.co.il/wp-content/uploads/2024/09/15/15/DSC_4930-960x640.jpg',
       source: 'IH',
-      publishDate: '2023-08-10'
-    }
+      publishDate: '2024-09-15'
+    },
+    {
+    
+      id: 8,
+      title: 'לאן צפוי להגיע ענף שטחי העבודה ואיך זה קשור לפקקים?',
+      link: 'https://nadlan.walla.co.il/item/3328167',
+      description: 'תחזיות אודות שוק המשרדים ועל עליית ערך הנדלן בגוש דן',
+      imageUrl: 'https://images.wcdn.co.il/f_auto,q_auto,w_700/2/9/5/8/2958746-46.png', // Replace with actual image if available
+      source: 'Walla',
+      publishDate: '2019-12-07' // Replace with actual publish date if available
+    },
   ];
 
 
